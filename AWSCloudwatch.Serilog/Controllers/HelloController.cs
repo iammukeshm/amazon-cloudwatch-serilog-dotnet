175db43cbf0b8e76a4925e4869db25f9e6285392
Mukesh Murugan <iammukeshm@gmail.com>
2022-11-26 16:11:32 +0530

    Add project files.

---
+++ AWSCloudwatch.Serilog/Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AWSCloudwatch.Serilog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        private readonly ILogger<HelloController> _logger;
+
+        public HelloController(ILogger<HelloController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult GetAsync(int id)
+        {
+            _logger.LogDebug("Received Request with id as {id}", id);
+            _logger.LogInformation("Processing your request");
+            _logger.LogError("Some Errors occcured.");
+            return Ok("Logged");
+        }
+    }
+}
